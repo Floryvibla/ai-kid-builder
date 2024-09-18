cb093219af5942fbf76910ac8947dd2964d6b6e6
Flory <flory.contato@gmail.com>
2024-09-18 12:07:52 -0300

    Add tabs e add card active

--- src/app/dashboard/hero.tsx
+++ src/app/dashboard/hero.tsx
@@ -1,9 +1,33 @@
+'use client'
+
+
 import { CarouselBox } from '@/components/carousel-section'
 import { ModalView } from '@/components/modal-view'
 import Image from 'next/image'
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+const films1 = [
+  "https://blackoutv.com/wp-content/uploads/2023/12/7KkHiZMvEdEZq2qrQX3kzYA7Off-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/3iS58y1wz2JVJ57HnI9D92fsYYh-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/c6zUhFMjZI63sHr3S9wIbddjAFC-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/jJJuiyH3ibOA3VdUjrXA88HRxfS-scaled-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/eBU28W7OWFxhKluBQDHsKLFyL5p-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2024/01/8Wis3WRAb8BjfSiz2ATMDBXFtpC-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2024/01/8Wis3WRAb8BjfSiz2ATMDBXFtpC-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/u3jDgXLcbYH5EfrzghJGwsZRVnZ-288x400.jpg"
+]
+
+const films2 = [
+  "https://blackoutv.com/wp-content/uploads/2023/12/eBU28W7OWFxhKluBQDHsKLFyL5p-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2024/01/8Wis3WRAb8BjfSiz2ATMDBXFtpC-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2024/01/8Wis3WRAb8BjfSiz2ATMDBXFtpC-288x400.jpg",
+  "https://blackoutv.com/wp-content/uploads/2023/12/u3jDgXLcbYH5EfrzghJGwsZRVnZ-288x400.jpg"
+]
 
 export function HeroStory() {
+  const [tabActive, setTabActive] = React.useState<'my_stories' | 'explorer'>('my_stories')
+
   return (
     <>
       <section className='text-white px-[10vw] -z-10 py-[10vh] flex flex-col h-[90%]  absolute top-0 left-0 w-full'>
@@ -15,7 +39,19 @@ export function HeroStory() {
         />
       </section>
       <div className='flex flex-col px-[5%]'>
-        <CarouselBox/>
+        <Tabs defaultValue={tabActive} className="">
+          <TabsList className='border backdrop-blur-md bg-black/40'>
+            <TabsTrigger value="my_stories">Minhas histórias</TabsTrigger>
+            |
+            <TabsTrigger value="explorer">Explorar</TabsTrigger>
+          </TabsList>
+          <TabsContent value="my_stories">
+            <CarouselBox items={films1}/>
+          </TabsContent>
+          <TabsContent value="explorer">
+            <CarouselBox items={films2}/>
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   )
--- src/components/carousel-section.tsx
+++ src/components/carousel-section.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import * as React from "react"
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card } from "@/components/ui/card"
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import Image from "next/image"
+import clsx from "clsx"
 
 const films = [
   "https://blackoutv.com/wp-content/uploads/2023/12/7KkHiZMvEdEZq2qrQX3kzYA7Off-288x400.jpg",
@@ -21,9 +25,16 @@ const films = [
   "https://blackoutv.com/wp-content/uploads/2023/12/u3jDgXLcbYH5EfrzghJGwsZRVnZ-288x400.jpg"
 ]
 
-export function CardStory({item}:{item:any}) {
+interface PropsCard {
+  items: any[]
+  // isActive: boolean
+}
+
+export function CardStory({item, isActive}:{item:any, isActive: boolean}) {
   return (
-    <Card className="h-[300px] md:h-[315px] relative cursor-pointer hover:opacity-90">
+    <Card className={clsx("h-[300px] md:h-[315px] relative cursor-pointer hover:opacity-90", {
+      'border-primary border-4': isActive
+    })}>
       <Image 
         src={item}
         alt=""
@@ -35,24 +46,54 @@ export function CardStory({item}:{item:any}) {
   )
 }
 
-export function CarouselBox() {
+export function CarouselBox({ items }: PropsCard) {
+
+  const [cardIndex, setCardIndex] = React.useState(0)
+  const [api, setApi] = React.useState<CarouselApi>()
+  const [current, setCurrent] = React.useState(0)
+  const [count, setCount] = React.useState(0)
+
+  const handleOnclickCard = (index:number) => {
+    setCardIndex(index)
+  }
+
+  React.useEffect(() => {
+    if (!api) {
+      return
+    }
+ 
+    setCount(api.scrollSnapList().length)
+    setCardIndex(api.selectedScrollSnap())
+ 
+    api.on("select", (teste) => {
+      console.log("Yeees: ", teste.canScrollNext());
+      
+      setCardIndex(api.selectedScrollSnap())
+    })
+  }, [api])
+
   return (
     <Carousel
       opts={{
         align: "start",
       }}
       className="w-full"
+      setApi={setApi}
     >
       <div className="relative w-[100%] flex justify-end gap-5">
         <CarouselPrevious className="" />
         <CarouselNext className="" />
       </div>
-      <div className="w-full mask-linear mask-dir-to-r  mask-point-from-[80%]">
+      <div className={clsx("w-full", {'mask-linear mask-dir-to-r  mask-point-from-[80%]': (count-1) !== cardIndex})}>
         <CarouselContent className="">
-          {films.map((item, index) => (
-            <CarouselItem key={index} className="basis-[220px] lg:basis-[227px]">
+          {items.map((item, index) => (
+            <CarouselItem 
+              key={index} 
+              onClick={() => handleOnclickCard(index)}
+              className="basis-[220px] lg:basis-[227px]"
+            >
               <div className="p-1">
-                <CardStory item={item}/>
+                <CardStory item={item} isActive={index === cardIndex}/>
               </div>
             </CarouselItem>
           ))}

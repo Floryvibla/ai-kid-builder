64bb2d09bb9b80704a6ca9cd98ef92d523559720
Flory <flory.contato@gmail.com>
2024-10-06 23:32:09 -0300

    Criando o banner da pagina de visualização de historia

--- src/app/dashboard/story/[id]/page.tsx
+++ src/app/dashboard/story/[id]/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default async function Page() {
   return (
-    <div>Page</div>
+    <main className='text-white'>
+      <div className='flex w-full h-[140px] md:h-[400px] relative'>
+        <div className=' flex-1 flex flex-col justify-center'>
+          <div className='px-5 md:px-20 gap-3 md:gap-6 absolute flex flex-col justify-center'>
+            <h1 className='text-xl md:text-5xl'>Rebel Ridge Ridge Ridge</h1>
+            <Button asChild className='px-5 w-[120px] md:w-[220px] md:text-2xl'> 
+              <Link href={'#'}>ASSISTIR</Link>
+            </Button>
+          </div>
+        </div>
+        <div className='relative w-[50%] flex-[3]'>
+          <Image 
+            src={'/wal.jpg'} 
+            alt='' 
+            fill 
+            className='-z-50 object-cover mask-linear mask-dir-to-l mask-from-[1] mask-point-from-[10%]' 
+          />
+        </div>
+      </div>
+    </main>
   )
 }
--- src/components/button-client.tsx
+++ src/components/button-client.tsx
@@ -5,12 +5,18 @@ import { useRouter } from 'next/navigation'
 import React from 'react'
 import { Button } from './ui/button'
 
-export default function ButtonAuthCTA() {
+interface Props {
+  children?: React.ReactNode
+  asChild?: boolean
+}
+
+export default function ButtonAuthCTA({children= 'Criar história', asChild}:Props) {
 
   const { data:session } = useSession()
   const router = useRouter()
   
   const handleSign = () => {
+    if(asChild) return null
     if(session) return router.push('/dashboard')
     signIn('google', { callbackUrl: "/dashboard" })
   }
@@ -22,8 +28,9 @@ export default function ButtonAuthCTA() {
     <Button
       className='bg-purple-700 mt-4 text-lg md:text-2xl font-bold md:p-7 md:rounded-2xl shadow-2xl hover:bg-purple-700/90'
       onClick={handleSign}
+      asChild={asChild}
     >
-      Criar história
+      {children}
     </Button>
   )
 }
